ea8ee6d1c5264fdcd4d53566e04afb5494b8bb10
Rohit Deka <studyrhd@gmail.com>
2025-07-31 23:04:49 +0530

    new

--- Java-DSA/day6.java
+++ Java-DSA/day6.java
@@ -1,3 +1,15 @@
+import java.util.Stack;
+
 public class day6 {
-    
+    public static void main(String[] args) {
+        
+        int[] arr= {2,4,6,8,10};
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print(arr[i]+""+ arr[j] + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
 }
