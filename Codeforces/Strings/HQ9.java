c3d97f781ce41b842d6a47c04b237c03848e242c
rohitdeka-1 <studyrhd@gmail.com>
2025-09-04 13:04:17 +0530

    Commit 1

---
+++ Codeforces/Strings/HQ9.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class HQ9 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        if (name.contains("H") || name.contains("Q") || name.contains("9")) {
+            System.out.print("YES");
+        } else {
+            System.out.print("NO");
+        }
+
+    }
+}
