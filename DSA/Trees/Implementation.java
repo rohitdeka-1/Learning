159e1e1125464e91104b0f9e1a9192efa25a8ac3
Rohit Deka <rohit.23MIM10093@vitbhopal.ac.in>
2026-07-30 00:17:46 +0530

    new

--- DSA/Trees/Implementation.java
+++ DSA/Trees/Implementation.java
@@ -24,6 +24,31 @@ public class Implementation {
         display(root.right);
 
     }
+
+    public static void preOrder(Node root){
+        if(root == null) return;
+
+        System.out.println(root.val);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static int size_nodes(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        return size_nodes(root.left) + size_nodes(root.right) + 1;
+
+    }
+
+    public static int sum_tree(Node root){
+        if(root == null) return  0;
+
+        return sum_tree(root.left) + sum_tree(root.right) + root.val;
+    }
+
+    public static 
     
     public static void main(String[] args){
         Node root = new Node(2);
@@ -33,15 +58,19 @@ public class Implementation {
         root.right = b;
 
         Node c = new Node(6);
-        Node d = new Node(11);
+        Node d = new Node(5 );
         a.left = c;
         a.right = d;
 
         Node e = new Node(11);
         b.right = e;
         
-        display(root);
+        // preOrder(root);
+        int num = size_nodes(root);
+        System.out.println(num);
 
+        int sum = sum_tree(root);
+        System.out.println(sum);
     }
 
 }
